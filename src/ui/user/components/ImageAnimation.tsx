0cb3fdd25e81758c00efa5137cf36a60eef00d1e
suekroos <suemori13@gmail.com>
2022-10-24 23:16:00 +0900

    複数の画像を自動的に差し替えるCSSアニメーションの実装途中

--- src/ui/user/components/ImageAnimation.tsx
+++ src/ui/user/components/ImageAnimation.tsx
@@ -1,12 +1,14 @@
 import medical_image1 from '../../images/medical_bear.jpg'
 import medical_image2 from '../../images/medical_ope.jpg'
-import medical_image3 from '../../images/medical_women.jpg'
+import medical_image3 from '../../images/medical_woman.jpg'
 
 export const ImageAnimation = () => {
     return(
         <div className='flex justify-center '>
-            <div className='w-full h-full'>
-                <img src={medical_image1} alt='medical_image' className='animate-popup w- h-full' />
+            <div className=''>
+                <img src={medical_image1} alt='medical_image' className='animate-popup img_auto' width='600' />
+                <img src={medical_image2} alt='medical_image' className='animate-popup img_auto' width='600' />
+                <img src={medical_image3} alt='medical_image' className='animate-popup img_auto' width='600' />
             </div>
         </div>
        
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
         animation:{
-          'popup': 'popup 8s cubic-bezier(0.22, 1, 0.36, 1) 1 forwards'
+          'popup': 'popup 8s cubic-bezier(0.22, 1, 0.36, 1) 1 forwards',
+          'img_auto': 'change-img-anime 30s infinite',
         },
       keyframes:{
         'popup':{
@@ -22,9 +23,25 @@ module.exports = {
           '80%, 100%' :{
             opacity: '1',
           },
+        },
+        'change-img-anime':{
+          '0%' :{
+            opacity: '0', 
+          },
+          '10%' :{
+            opacity: '1', 
+          },
+          '90%' :{
+            opacity: '1', 
+          },
+          '100%' :{
+            opacity: '0', 
+          },
         }
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animation-delay'),
+  ],
 }
