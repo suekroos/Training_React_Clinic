31915cc57780b91b448e62508e36f3f55828f004
suekroos <suemori13@gmail.com>
2022-10-26 23:40:40 +0900

    カスタムアニメーションの作成

--- src/ui/user/components/ImageAnimation.tsx
+++ src/ui/user/components/ImageAnimation.tsx
@@ -5,11 +5,13 @@ import medical_image3 from '../../images/medical_woman.jpg'
 export const ImageAnimation = () => {
     return(
         <div className='flex justify-center '>
-            <div className='relative w-96 h-96 max-w-full'>
+            <div className='relative w-900 h-700 max-w-full '>
                 <img src={medical_image1} alt='medical_image' className='img-auto-anime' />
                 <img src={medical_image2} alt='medical_image' className='img-auto-anime' />
                 <img src={medical_image3} alt='medical_image' className='img-auto-anime' />
             </div>
+            {/* <div className="bg-medical_image1">
+            </div> */}
         </div>
        
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,62 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx,css}",],
   theme: {
     animationDelay: {
       0: "0s",
-      10: "1s",
-      20: "2s",
+      10: "3s",
+      20: "6s",
     },
     extend: {
         animation:{
-          'popup': 'popup 8s cubic-bezier(0.22, 1, 0.36, 1) 1 forwards',
-          'img_auto': 'change-img-anime 3s infinite',
+          // 'popup': 'popup 3s cubic-bezier(0.22, 1, 0.36, 1) infinite',
+          'img-auto': 'change-img-anime 9s infinite',
         },
+        width:{
+          '900': '900px'
+        },
+        height:{
+          '700': '700px'
+        },
+        // backgroundImage:{
+        //   'medical_image1': "url('../../images/medical_bear.jpg')",
+        //   'medical_image2': "url('./src/ui/images/medical_ope.jpg')",
+        //   'medical_image3': "url('./src/ui/images/medical_woman.jpg')",
+        // },
       keyframes:{
-        'popup':{
-          '0%' :{
-            transform: 'translateY(0)',
-            opacity: '0'
-          },
-          '0%, 100%' :{
-            transform: 'scale(1.0)',
+        // 'popup':{
+        //   '0%' :{
+        //     transform: 'translateY(0)',
+        //     opacity: '0'
+        //   },
+        //   '0%, 100%' :{
+        //     transform: 'scale(1.0)',
             
-          },
-          '100%' :{
-            transform: 'translateY(0)',
-          },
-          '80%, 100%' :{
-            opacity: '1',
-          },
-        },
+        //   },
+        //   '100%' :{
+        //     transform: 'translateY(0)',
+        //   },
+        //   '80%, 100%' :{
+        //     opacity: '1',
+        //   },
+        // },
         'change-img-anime':{
           '0%' :{
             opacity: '0', 
           },
-          '10%' :{
+          '30%' :{
             opacity: '1', 
           },
-          '90%' :{
+          '36%' :{
             opacity: '1', 
+          },
+          '45%' :{
+            opacity: '0', 
           },
           '100%' :{
             opacity: '0', 
           },
-        }
+        },
       },
     },
   },
