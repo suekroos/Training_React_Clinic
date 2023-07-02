bcb1d732f22a6222c9a89e61d04a914d5959ce97
suekroos <suemori13@gmail.com>
2023-07-02 18:53:47 +0900

    挨拶項目の修正

--- src/ui/user/components/Greeting.tsx
+++ src/ui/user/components/Greeting.tsx
@@ -1,9 +1,27 @@
+import PlayArrowOutlinedIcon from "@mui/icons-material/PlayArrowOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import thank from "../../images/Thankyou.png";
+
 export const Greeting = () => {
   return (
-    <div className="m-auto flex h-96 max-w-screen-lg bg-doctor bg-cover">
-      <div className="box-border flex h-96 w-96 flex-col justify-center bg-cover">
+    <div className="m-auto mt-20 block h-96 max-w-screen-lg bg-doctor bg-cover">
+      <div className="absolute right-40 ">
+        <div className="rounded-lg border-2 border-blue-400 pt-10 pr-5 pb-10 pl-5 text-blue-400 [writing-mode:vertical-rl]">
+          はじめまして。
+          <br />
+          院長の末盛です。
+        </div>
+      </div>
+      <div className="relative box-border flex h-96 w-96 flex-col justify-center bg-cover">
         <div>
-          <p className="rounded-t-md text-xl">
+          <img
+            src={thank}
+            alt="thank-image"
+            className="absolute -left-14 top-5 block opacity-20"
+          ></img>
+        </div>
+        <div>
+          <p className="-mb-5 rounded-t-md text-xl font-bold">
             ごあいさつ
             <br />
             <br />
@@ -16,8 +34,13 @@ export const Greeting = () => {
             末盛胃腸内視鏡・肛門クリニックをどうぞよろしくお願い致します。
           </p>
         </div>
+        <div className="btn-detail text-center">
+          <a href="" className="block">
+            クリニック紹介
+            <PlayArrowOutlinedIcon className="absolute right-3 mt-1" />
+          </a>
+        </div>
       </div>
-      <div className="">aa</div>
     </div>
   );
 };
